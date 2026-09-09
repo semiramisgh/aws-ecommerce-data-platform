715ba29e387aa4f3222885eac05e0cf618e9aa9a
Samira Ghareh Gozlou <gh.edu.uk@gmail.com>
2026-09-09 15:17:32 +0200

    feat: manage Glue ETL jobs with Terraform

--- infrastructure/crawlers.tf
+++ infrastructure/crawlers.tf
@@ -1,67 +1,67 @@
 resource "aws_glue_crawler" "clean" {
-    name = "ecommerce-clean-crawler"
-    description = "Catalog clean Parquet datasets from Amazon S3"
-    database_name = aws_glue_catalog_database.clean.name
-    role = "AWSGlueServiceRole-ecommerce-data-platform"
+  name          = "ecommerce-clean-crawler"
+  description   = "Catalog clean Parquet datasets from Amazon S3"
+  database_name = aws_glue_catalog_database.clean.name
+  role          = "AWSGlueServiceRole-ecommerce-data-platform"
 
-    configuration = jsonencode({
-        Version = 1.0
-        CreatePartitionIndex = true
-    })
+  configuration = jsonencode({
+    Version              = 1.0
+    CreatePartitionIndex = true
+  })
 
-    lineage_configuration {
-        crawler_lineage_settings = "DISABLE"
-    }
+  lineage_configuration {
+    crawler_lineage_settings = "DISABLE"
+  }
 
-    recrawl_policy {
-        recrawl_behavior = "CRAWL_EVERYTHING"
-    }
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_EVERYTHING"
+  }
 
-    schema_change_policy {
-        update_behavior = "UPDATE_IN_DATABASE"
-        delete_behavior = "DEPRECATE_IN_DATABASE"
-    }
+  schema_change_policy {
+    update_behavior = "UPDATE_IN_DATABASE"
+    delete_behavior = "DEPRECATE_IN_DATABASE"
+  }
 
-    s3_target {
-        path = "s3://aws-ecommerce-data-platform-sg-20260904/clean/"
-        exclusions = []
-    }
+  s3_target {
+    path       = "s3://aws-ecommerce-data-platform-sg-20260904/clean/"
+    exclusions = []
+  }
 
-    lifecycle {
-        prevent_destroy = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_glue_crawler" "curated" {
-    name = "ecommerce-curated-crawler"
-    description = "Catalog curated sales Parquet dataset from Amazon S3"
-    database_name = aws_glue_catalog_database.curated.name
-    role = "AWSGlueServiceRole-ecommerce-data-platform"
+  name          = "ecommerce-curated-crawler"
+  description   = "Catalog curated sales Parquet dataset from Amazon S3"
+  database_name = aws_glue_catalog_database.curated.name
+  role          = "AWSGlueServiceRole-ecommerce-data-platform"
 
-    configuration = jsonencode({
-        Version = 1.0
-        CreatePartitionIndex = true
-    })
+  configuration = jsonencode({
+    Version              = 1.0
+    CreatePartitionIndex = true
+  })
 
-    lineage_configuration {
-        crawler_lineage_settings = "DISABLE"
-    }
+  lineage_configuration {
+    crawler_lineage_settings = "DISABLE"
+  }
 
-    recrawl_policy {
-        recrawl_behavior = "CRAWL_EVERYTHING"
-    }
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_EVERYTHING"
+  }
 
-    schema_change_policy {
-        update_behavior = "UPDATE_IN_DATABASE"
-        delete_behavior = "DEPRECATE_IN_DATABASE"
-    }
+  schema_change_policy {
+    update_behavior = "UPDATE_IN_DATABASE"
+    delete_behavior = "DEPRECATE_IN_DATABASE"
+  }
 
-    s3_target {
-        path = "s3://aws-ecommerce-data-platform-sg-20260904/curated/sales"
-        exclusions = []
-    }
+  s3_target {
+    path       = "s3://aws-ecommerce-data-platform-sg-20260904/curated/sales"
+    exclusions = []
+  }
 
-    lifecycle {
-        prevent_destroy = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
---
+++ infrastructure/jobs.tf
@@ -0,0 +1,78 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_role" "glue_service" {
+  name = "AWSGlueServiceRole-ecommerce-data-platform"
+}
+
+locals {
+  glue_common_default_arguments = {
+    "--spark-event-logs-path" = "s3://aws-glue-assets-${data.aws_caller_identity.current.account_id}-${var.aws_region}/sparkHistoryLogs/"
+    "--S3_BUCKET_NAME"        = var.s3_bucket_name
+    "--enable-job-insights"   = "false"
+    "--conf"                  = "spark.sql.catalog.glue_catalog.glue.skip-name-validation=true"
+    "--job-bookmark-option"   = "job-bookmark-disable"
+    "--job-language"          = "python"
+    "--TempDir"               = "s3://${var.s3_bucket_name}/glue-temp/"
+  }
+}
+
+resource "aws_glue_job" "raw_to_clean" {
+  name              = "aws-ecommerce-raw-to-clean"
+  role_arn          = data.aws_iam_role.glue_service.arn
+  glue_version      = "6.0"
+  worker_type       = "G.1X"
+  number_of_workers = 2
+  max_retries       = 0
+  timeout           = 10
+  execution_class   = "STANDARD"
+
+  default_arguments = merge(
+    local.glue_common_default_arguments,
+    {
+      "--enable-continuous-cloudwatch-log" = "true"
+    }
+  )
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${var.s3_bucket_name}/scripts/glue/glue-raw-to-clean.py"
+    python_version  = "3"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_glue_job" "clean_to_curated" {
+  name              = "aws-ecommerce-clean-to-curated"
+  description       = "Transforms clean Parquet datasets into curated"
+  role_arn          = data.aws_iam_role.glue_service.arn
+  glue_version      = "6.0"
+  worker_type       = "G.1X"
+  number_of_workers = 2
+  max_retries       = 0
+  timeout           = 10
+  execution_class   = "STANDARD"
+
+  default_arguments = local.glue_common_default_arguments
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${var.s3_bucket_name}/scripts/glue/aws-clean-to-curated.py"
+    python_version  = "3"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name]
+  }
+}
